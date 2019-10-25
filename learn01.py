8cedd013aa1a1066072d9a8d627185d759007e1a
YanFeng <yanfengemail@163.com>
2019-10-26 00:18:05 +0800

    完成第一章学习

--- learn01.py
+++ learn01.py
@@ -1,17 +1,18 @@
 from __future__ import print_function
 import torch as tc
+import numpy as np
 
-a = tc.empty(5,3)
-print(a)
+x = tc.empty(5,3)
+print(x)
 
-b = tc.rand(5,3)
-print(b)
+x = tc.rand(5,3)
+print(x)
 
-c = tc.zeros(5,3,dtype = tc.long)
-print(c)
+x = tc.zeros(5,3,dtype = tc.long)
+print(x)
 
-d = tc.tensor([5.5,3])
-print(d)
+x = tc.tensor([5.5,3])
+print(x)
 
 x = tc.zeros(5,3,dtype=tc.long)
 x = x.new_ones(5,3,dtype = tc.double)
@@ -31,3 +32,34 @@ print(result)
 y.add_(x)
 print(y)
 
+print(x[:,1])
+x = tc.randn(4,4)
+y = x.view(16)
+z = x.view(-1,8)
+print(x.size(),y.size(),z.size())
+
+x = tc.randn(1)
+print(x)
+print(x.item())
+
+a = tc.ones(5)
+print(a)
+b = a.numpy()
+print(b)
+a.add_(1)
+print(a)
+print(b)
+
+a = np.ones(5)
+b = tc.from_numpy(a)
+np.add(a,1,out=a)
+print(a)s
+print(b)
+
+if tc.cuda.is_available():
+    device = tc.device("cuda")
+    y = tc.ones_like(x,device=device)
+    x = x.to(device)
+    z = x + y
+    print(z)
+    print(z.to("cpu",tc.double))
