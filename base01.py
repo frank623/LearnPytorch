1f1dc9e0b95887abf9fc196401ec703100201680
YanFeng <yanfengemail@163.com>
2019-10-29 08:16:22 +0800

    Tensor 基本操作

--- base01.py
+++ base01.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 #定义一个三行两列给定元素的矩阵
 a = torch.Tensor([[2,3],[4,8],[7,9]])
@@ -14,3 +15,18 @@ print('c is : {}'.format(c))
 d = torch.randn(3,2)
 print('d is : {}'.format(d))
 
+a[0,1] = 100
+print('changed a is : {}'.format(a))
+#将tensor转为numpy
+numpy_b = b.numpy()
+print('conver to numpy is \n {}'.format(numpy_b))
+#将numpy转为tensor
+e = np.array([[2,3],[4,5]])
+torch_e = torch.from_numpy(e)
+print('from numpy to torch.Tensor is {}'.format(torch_e))
+f_torch_e = torch_e.float()
+print('change data type to float tensor：{}'.format(f_torch_e))
+
+if torch.cuda.is_available():
+    a_cuda = a.cuda()
+    print(a_cuda)
